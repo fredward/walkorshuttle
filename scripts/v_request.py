375c2f6f9015ddf3578c3c0abfded28defde0a8b
Carl Ward <carlcward@gmail.com>
2012-11-27 02:59:20 -0500

    script wasnt committed for some reason

--- scripts/v_request.py
+++ scripts/v_request.py
@@ -1,2 +1,8 @@
-import urllib, json
-params = urllib.urlencode({"agencies" : 52
+from subprocess import call
+import urllib, json, cPickle
+
+
+params = urllib.urlencode({"agencies" : 52})
+f = urllib.urlopen("http://api.transloc.com/1.1/vehicles.json?%s" % params)
+#print json.load(f)['data'][params['agencies']]
+call(['python','/Users/carlcward/Documents/cs50/walkorshuttle/manage.py','add_vehicle',cPickle.dumps(json.load(f))])
