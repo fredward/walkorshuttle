d866a1c86cb2bf8123bf70033bf1eea1a6eb2e0b
Carl Ward <carlcward@gmail.com>
2012-11-28 13:55:05 -0500

    added getting closest stop and shuttles for given lat,lon (Does not query user for location yet)

--- wosapp/views.py
+++ wosapp/views.py
@@ -1,11 +1,25 @@
 # Create your views here.
 
 from django.shortcuts import render
+from wosapp.packages.distance import haversine
+from wosapp.models import Vehicle, Route, Stop, Arrival_Estimate
 
-from wosapp.models import Vehicle, Route, Stop
-
-def index(response):
-	closest_stop =  Stop.objects.get(name__exact='Boylston Gate')
+def index(request):
+	lat = 42.381864
+	lon = -71.125383
+	# get the closest shuttle stop
+	stops  = Stop.objects.all()
+	min_dist = 0.0
+	if( len(stops) > 0 ):
+		min_dist = haversine(lat, lon, stops[0].location_lat, stops[0].location_lon)
+		min_stop = stops[0]
+		for stop in stops:
+			dist = haversine(lat, lon, stop.location_lat, stop.location_lon)
+			if(dist < min_dist):
+				min_dist = dist
+				min_stop = stop
+	
+	closest_stop =  min_stop
 	#for( veh in vehicles_running):
 	#	route = veh.route
 	
@@ -13,6 +27,7 @@ def index(response):
 	print possible_routes.values('id')
 	vehicles_running_to_stop = Vehicle.objects.filter(route__in=possible_routes)
 	#routes_in_service =  Route.objects.filter(id__in=vehicles_running_to_stop.values['route'])
-	
-	context = {'routes_in_service' : possible_routes, 'vehicles_running_to_stop' : vehicles_running_to_stop, 'closest_stop' : closest_stop}
-	return render(response, 'wosapp/index.html', context)
+	# get the next shuttles at the closest stop, ordered by time
+	arrival_estimates_at_stop = Arrival_Estimate.objects.filter(stop=min_stop).order_by('time')
+	context = {'routes_in_service' : possible_routes, 'vehicles_running_to_stop' : vehicles_running_to_stop, 'closest_stop' : closest_stop, 'next_shuttles' : arrival_estimates_at_stop}
+	return render(request, 'wosapp/index.html', context)
