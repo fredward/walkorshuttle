0ab242775529a13333a407d23862101d1891215b
Fred Ward <ultimate.fw@gmail.com>
2012-11-27 23:28:00 -0500

    first temple and basic site setup

--- wosapp/views.py
+++ wosapp/views.py
@@ -1,1 +1,10 @@
 # Create your views here.
+
+from django.shortcuts import render
+
+from wosapp.models import Vehicle
+
+def index(response):
+	vehicles_running = Vehicle.objects.all()
+	context = {'vehicles_running' : vehicles_running}
+	return render(response, 'wosapp/index.html', context)
