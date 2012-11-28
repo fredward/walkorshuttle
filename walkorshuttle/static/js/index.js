3bab0fa39b99dcc30690ad3cbfa4191223852edc
Fred Ward <frederickrward@gmail.com>
2012-11-28 10:49:57 -0500

    more changes to route display scripts

--- walkorshuttle/static/js/index.js
+++ walkorshuttle/static/js/index.js
@@ -2,10 +2,8 @@
 $(document).ready( function() {
 
 	$(".route-info").click( function() {
-				 	console.log("test");
 		$(this).find(".extra-route-prompt").hide();
 		$(this).find(".extra-route-info").slideDown();
-
 	}); 
 
 });
