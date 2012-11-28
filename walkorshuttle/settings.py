11ec4dc4e78ea3840bdb5ed2f9594e0ead109f30
Fred Ward <frederickrward@gmail.com>
2012-11-28 00:42:34 -0500

    added bootstrap

--- walkorshuttle/settings.py
+++ walkorshuttle/settings.py
@@ -56,17 +56,19 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
 
+STATIC_ROOT = ''
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
 
+import os
+DIR = os.path.dirname(os.path.abspath(__file__))
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(DIR, 'static'),
 )
 
 # List of finder classes that know how to find static files in
