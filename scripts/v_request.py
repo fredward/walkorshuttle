ee00933d6c165003d1429d9503e5de3e7ce8ee1f
Carl Ward <carlcward@gmail.com>
2012-12-06 16:37:04 -0500

    error handling in the transloc queries

--- scripts/v_request.py
+++ scripts/v_request.py
@@ -10,8 +10,10 @@ for i in range(60):
 		params = urllib.urlencode({"agencies" : 52})
 		f = urllib.urlopen("http://api.transloc.com/1.1/vehicles.json?%s" % params)
 		#print json.load(f)['data'][params['agencies']]
-		call(['python',manager_path,'add_vehicle',cPickle.dumps(json.load(f))])
-		print("Success!\t%s" % i)
-		sleep(1)
+		stuff = json.load(f)
+		if 'data' in stuff:
+			call(['python',manager_path,'add_vehicle',cPickle.dumps(stuff)])
+			print("Success!\t%s" % i)
+		sleep(.25)
 	except:
 		continue
