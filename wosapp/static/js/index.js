66a6341df795876cb5d1695a10f448aa3e32cf60
Fred Ward <frederickrward@gmail.com>
2013-05-04 11:27:01 -0400

    removed some thats hosted on S3

--- wosapp/static/js/index.js
+++
@@ -1,9 +0,0 @@
-//show more route info
-$(document).ready( function() {
-
-	$(".route-info").click( function() {
-		$(this).find(".extra-route-prompt").hide();
-		$(this).find(".extra-route-info").slideDown();
-	}); 
-
-});
